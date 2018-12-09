38d97b06b537673c40d45a764d92e548f2b3467b
Petr Benes <w.benny@outlook.com>
2018-12-09 23:20:49 +0100

    broader definition of ipi_call() function

--- src/hvpp/hvpp/lib/mp.h
+++ src/hvpp/hvpp/lib/mp.h
@@ -24,35 +24,21 @@ namespace mp
   inline void sleep(uint32_t milliseconds) noexcept
   { detail::sleep(milliseconds); }
 
+  //
+  // Inter-Processor Interrupt - runs specified method on all logical CPUs.
+  //
+
   inline void ipi_call(void(*callback)(void*), void* context) noexcept
   { detail::ipi_call(callback, context); }
 
   inline void ipi_call(void(*callback)()) noexcept
   { detail::ipi_call([](void* context) { ((void(*)())context)(); }, callback); }
 
-  template <
-    typename T
-  >
+  template <typename T>
+  inline void ipi_call(T function) noexcept
+  { ipi_call([](void* context) noexcept { ((T*)context)->operator()(); }, &function); }
+
+  template <typename T>
   inline void ipi_call(T* instance, void (T::*member_function)()) noexcept
-  {
-    //
-    // Inter-Processor Interrupt - runs specified method on all logical CPUs.
-    //
-    struct ipi_ctx
-    {
-      T* instance;
-      void (T::*member_function)();
-    } ipi_context {
-      instance,
-      member_function
-    };
-
-    ipi_call([](void* context) noexcept {
-      auto ipi_context = reinterpret_cast<ipi_ctx*>(context);
-      auto instance = ipi_context->instance;
-      auto member_function = ipi_context->member_function;
-
-      (instance->*member_function)();
-    }, &ipi_context);
-  }
+  { ipi_call([=]() { (instance->*member_function)(); }); }
 }
